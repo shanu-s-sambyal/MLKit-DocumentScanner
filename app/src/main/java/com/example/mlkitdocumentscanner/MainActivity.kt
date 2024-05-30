7195a3e8e430d4a50579d7ea7379746fac2011ca
shanu-s-sambyal <suryasambyal16@gmail.com>
2024-05-30 06:45:29 +0530

    issues needed to be fixed

--- app/src/main/java/com/example/mlkitdocumentscanner/MainActivity.kt
+++ app/src/main/java/com/example/mlkitdocumentscanner/MainActivity.kt
@@ -1,47 +1,130 @@
 package com.example.mlkitdocumentscanner
 
+import android.content.IntentSender
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.IntentSenderRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateMapOf
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
+import coil.compose.AsyncImage
 import com.example.mlkitdocumentscanner.ui.theme.MLKitDocumentScannerTheme
+import com.google.mlkit.vision.documentscanner.GmsDocumentScanning
+//import com.google.mlkit.vision.documentscanner.GmsDocumentScanner
+import com.google.mlkit.vision.documentscanner.GmsDocumentScannerOptions
+import com.google.mlkit.vision.documentscanner.GmsDocumentScanningResult
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
+        val options = GmsDocumentScannerOptions.Builder()
+            .setScannerMode(GmsDocumentScannerOptions.SCANNER_MODE_FULL)
+            .setGalleryImportAllowed(true)
+            .setPageLimit(5)
+            .setResultFormats(GmsDocumentScannerOptions.RESULT_FORMAT_JPEG , GmsDocumentScannerOptions.RESULT_FORMAT_PDF)
+            .build()
+        val scanner = GmsDocumentScanning.getClient(options)
         setContent {
             MLKitDocumentScannerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                            name = "Android",
-                            modifier = Modifier.padding(innerPadding)
+                Surface (
+                    modifier = Modifier.fillMaxSize() ,
+                    color = MaterialTheme.colorScheme.background
+                ){
+                    var imageUris by remember{
+                        mutableStateOf<List<Uri>>(emptyList())
+                    }
+                    val scannerLauncher = rememberLauncherForActivityResult(
+                        contract =  ActivityResultContracts.StartIntentSenderForResult(),
+                        onResult = {
+                            if(it.resultCode == RESULT_OK){
+                                val result = GmsDocumentScanningResult.fromActivityResultIntent(it.data)
+                                imageUris = result?.pages?.map {it.imageUri}?: emptyList()
+
+                                result?.pdf?.let {pdf->
+                                    val fos = FileOutputStream(File(filesDir , "scan.pdf"))
+                                    contentResolver.openInputStream(pdf.uri)?.use{
+                                        it.copyTo(fos)
+                                    }
+                                }
+                            }
+                        }
+
                     )
+
+                    Column(
+                        Modifier
+                            .fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ){
+                        imageUris.forEach{uri->
+                            AsyncImage(
+                                model = uri,
+                                contentDescription = null,
+                                contentScale = ContentScale.FillWidth,
+                                modifier = Modifier.fillMaxWidth()
+                            )
+                        }
+                        Button(onClick = {
+                            scanner.getStartScanIntent(this@MainActivity)
+                                .addOnSucessListener{
+                                    scannerLauncher.launch(
+                                        IntentSenderRequest.Builder(it)
+                                            .build()
+                                    )
+                                }
+                                .addOnFailureListener{
+                                    Toast.makeText(
+                                        applicationContext ,
+                                        it.message ,
+                                        Toast.LENGTH_LONG,
+                                    ).show()
+                                }
+                        }) {
+                            Text(text = "Scan Pdf")
+                        }
+                    }
                 }
+
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MLKitDocumentScannerTheme {
-        Greeting("Android")
-    }
-}
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Text(
+//            text = "Hello $name!",
+//            modifier = modifier
+//    )
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    MLKitDocumentScannerTheme {
+//        Greeting("Android")
+//    }
+//}
